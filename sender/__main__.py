0da2b8e931e3beaa3ddeefd0f4ade189d19397bf
Joynal Abedin <joynal.uu@gmail.com>
2022-01-25 07:30:08 +0100

    update

---
+++ sender/__main__.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+
+from parser.app import Application
+from parser.settings import load
+
+
+def setup_logging(log_level):
+    logger = logging.getLogger()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.setLevel(log_level)
+
+
+async def main():
+    settings = load("./settings.yaml")
+
+    setup_logging(log_level=settings.worker.log_level)
+    app = Application(settings=settings)
+
+    try:
+        await app.run()
+    except KeyboardInterrupt:
+        app.stop()
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
